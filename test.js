e6515798244c68cd709b53284c7cea12b4da05a3
Alexis <61119747+Alexis06030631@users.noreply.github.com>
2022-04-12 20:18:04 +0200

    Test app

--- test.js
+++ test.js
@@ -1,3 +1,19 @@
-var Sncf = require('./index')
-const sncf = new Sncf()
-sncf.login().then(r => console.log(r))
+const {Client, Places} = require('./index')
+const sncf = new Client()
+
+// Connect to sncf API with your credentials
+sncf.login("YOUR TOKEN").then(async () =>{
+
+    // Get all stations including the name "Paris
+    const FoundGare = await Places.found('Paris')
+
+    // Get all the information about the first station with the name "Paris".
+    const GetGareInfos = await Places.get(FoundGare.places[0].id)
+
+    // Return the data
+    console.log(GetGareInfos)
+
+}).catch(err => {
+    // Return errors
+    console.log(err)
+})
