275c2ed725d4360bae59f6a861c1bf7ee94cbe5b
Alexis <61119747+Alexis06030631@users.noreply.github.com>
2022-06-17 03:24:32 +0200

    places rework functions

--- src/places.js
+++ src/places.js
@@ -1,28 +1,48 @@
-const response_format = require("./utils/response_format");
-const Sncf = require("./client");
+const response_format = require("./data/response_format");
+const axios = require("axios");
+const utils = require("./utils");
 
-class Places extends Sncf {
-    async search(station, filter = true) {
-        const response = await this.requests('GET',`places/?q=${station}`)
-        if(response.data.places) {
-            if(filter) {
-                response.data.places = response.data.places.filter((d)=> d.embedded_type === 'stop_area')
-            }
-            return response_format.places(response.status, response.data.places)
-        } else {
-            return response_format.places(404, {message: 'No places found'})
-        }
+class Places {
+    #token
+    constructor(token) {
+        this.#token = token;
+    }
+
+
+    search(station, filter = true) {
+        return new Promise((resolve, reject) => {
+            axios({
+                method: 'GET',
+                url: utils.SNCFapi + `places/?q=${station}`,
+                headers: {
+                    'Authorization': this.#token
+                }
+            }).then(res => {
+                resolve(res.data)
+            }).catch(err => {
+                reject(utils.error(err.response));
+            })
+        })
     }
 
     async get(stationID){
-        if(!stationID.includes('stop_area:SNCF:')){
-            stationID = `stop_area:SNCF:${stationID}`
-        }
-        const response = await this.requests('GET', `places/${stationID}`)
-        if(!response.data.places) return response_format.place(404, {message: 'Place not found'})
-        else return response_format.place(response.status, response.data.places[0])
+        return new Promise((resolve, reject) => {
+            if(!stationID.includes('stop_area:SNCF:')){
+                stationID = `stop_area:SNCF:${stationID}`
+            }
+            axios({
+                method: 'GET',
+                url: utils.SNCFapi + `places/${stationID}`,
+                headers: {
+                    'Authorization': this.#token
+                }
+            }).then(res => {
+                resolve(res.data)
+            }).catch(err => {
+                reject(utils.error(err.response));
+            })
+        })
     }
 }
 
-Sncf.prototype.places = new Places()
-module.exports.Places = new Places()
+module.exports = Places;
