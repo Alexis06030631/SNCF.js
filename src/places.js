1efdce632f3115228606415db64046eb7faaee93
Alexis <61119747+Alexis06030631@users.noreply.github.com>
2022-07-19 19:32:03 +0200

    places add verifs

--- src/places.js
+++ src/places.js
@@ -9,7 +9,7 @@ class Places {
     }
 
 
-    search(station, filter = true) {
+    search(station) {
         return new Promise((resolve, reject) => {
             axios({
                 method: 'GET',
@@ -20,15 +20,19 @@ class Places {
             }).then(res => {
                 resolve(this._placesMany(res.data))
             }).catch(err => {
-                console.log(err)
-                reject(utils.error(err.response));
+                reject(utils.error(err))
             })
         })
     }
 
     async get(stationID){
         return new Promise((resolve, reject) => {
-            if(!stationID.includes('stop_area:SNCF:')){
+            if(stationID.length === 0) {
+                reject(new Error(Error.code.ID_MISSING))
+            }else if(!stationID.includes('stop_area:SNCF:')){
+                if(isNaN(Number(stationID))) {
+                    reject(new Error(Error.code.ID_IS_NOT_A_NUMBER))
+                }
                 stationID = `stop_area:SNCF:${stationID}`
             }
             axios({
@@ -40,7 +44,7 @@ class Places {
             }).then(res => {
                 resolve(new Place(res.data.places[0], this.#token))
             }).catch(err => {
-                reject(utils.error(err.response));
+                reject(utils.error(err));
             })
         })
     }
@@ -49,6 +53,9 @@ class Places {
 
 
     _placesMany(places) {
+        if(!places?.places?.length) {
+            return new Error(Error.code.PLACE_NOT_FOUND)
+        }
         const placesMany = [];
         for(let place of places.places) {
             placesMany.push(new Place(place, this.#token))
