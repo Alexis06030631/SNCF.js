2ac630e868140f3414a7ee850f5f7d6ef9edfc89
Alexis <61119747+Alexis06030631@users.noreply.github.com>
2022-07-19 23:00:46 +0200

    globalize requests

--- src/utils.js
+++ src/utils.js
@@ -1,33 +1,33 @@
-errors = [
-    {
-        code: 401,
-        message: 'Token invalid'
-    },
-    {
-        type: 'ID_INVALID',
-        message: 'The id is not valid'
-    },
-    {
-        type: 'ID_IS_NOT_A_NUMBER',
-        message: 'The id is not a number'
-    }
-]
-
+const axios = require("axios");
 module.exports = {
-    errors: errors.map(e => e.type),
     version: process.version,
     SNCFapi: 'https://api.navitia.io/v1/coverage/sncf/',
     SNCFapiVersion: 'v1',
     error: (error) => {
-        switch (error?.response?.code) {
+        switch (error?.response?.status) {
             case 401:
-                return new Error(Error.code.ID_INVALID)
+                return new Error(Error.code.TOKEN_INVALID)
 
             default:
                 return new Error(error)
         }
     },
 
-}
+    async request(token, url, method = 'GET') {
+        try {
+            const data = await axios({
+                method: method,
+                url: this.SNCFapi + url,
+                headers: {
+                    'Authorization': token
+                }
+            })
 
-// Doc errors code
+            return data.data
+        }catch (e) {
+            console.log(e)
+            throw this.error(e)
+        }
+    }
+
+}
