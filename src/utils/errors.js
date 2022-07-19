823d4ddee47bee056b0c3b62ad85057613768d03
Alexis <61119747+Alexis06030631@users.noreply.github.com>
2022-07-19 19:31:21 +0200

    add docs errors

---
+++ src/utils/errors.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+    TOKEN_INVALID: 'An invalid token was provided.',
+    TOKEN_MISSING: 'Request to use token, but token was unavailable to the client.',
+
+    ID_INVALID: 'The id is not valid.',
+    ID_IS_NOT_A_NUMBER: 'The id is not a number.',
+    ID_MISSING: 'The id is missing.',
+
+    PLACE_NOT_FOUND: 'The place was not found.',
+}
