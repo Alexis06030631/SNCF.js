dc947808b5a1dcedc75aaa7122cae7ab2b538189
Alexis <61119747+Alexis06030631@users.noreply.github.com>
2022-07-19 19:36:58 +0200

    test

--- test.js
+++ test.js
@@ -1,26 +1,15 @@
-const {Client, Routes, Places} = require('./index')
+const {Client} = require('./index')
 require("dotenv").config();
 const sncf = new Client()
 
 // Connect to sncf API with your credentials
 sncf.login().then(_ =>{
 
+    console.log(Object.getOwnPropertyNames(Object.getPrototypeOf(sncf.places)))
     // Get all stations including the name "Paris
-    sncf.places.search('Toulouse matabiau').then((res) => {
-        console.log(res.places[0])
-        sncf.places.get(res.places[0].id).then((res) => {
-            console.log(res)
-        })
+    sncf.places.search('Toulouse matabiau').then(async place => {
+        console.log(await place[0].disruptions())
     })
-
-    // Get all the information about the first station with the name "Paris".
-   // const GetGareInfos = await Places.get(FoundGare.places[0].id)
-
-    // Get the routes to a train station.
-    //const routes = await Routes.search('Toulouse', 'montauban')
-    //console.log(routes.routes[0].id)
-    //const route = await Routes.get(routes.routes[0].id)
-    //const stop_areas = await Routes.stop_areas(routes.routes[0].id)
 
 }).catch(err => {
     console.log(err)
