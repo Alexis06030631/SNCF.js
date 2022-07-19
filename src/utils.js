49f777e1b4d5dd38d78f31f0272c8a74df8534fb
Alexis <61119747+Alexis06030631@users.noreply.github.com>
2022-07-19 19:31:40 +0200

    rework errors system

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,33 @@
+errors = [
+    {
+        code: 401,
+        message: 'Token invalid'
+    },
+    {
+        type: 'ID_INVALID',
+        message: 'The id is not valid'
+    },
+    {
+        type: 'ID_IS_NOT_A_NUMBER',
+        message: 'The id is not a number'
+    }
+]
+
 module.exports = {
+    errors: errors.map(e => e.type),
     version: process.version,
     SNCFapi: 'https://api.navitia.io/v1/coverage/sncf/',
     SNCFapiVersion: 'v1',
     error: (error) => {
-        switch (error.code) {
+        switch (error?.response?.code) {
             case 401:
-                return new Error('TOKEN_INVALID');
+                return new Error(Error.code.ID_INVALID)
+
             default:
-                return new Error(error.data.message);
+                return new Error(error)
         }
-    }
+    },
+
 }
+
+// Doc errors code
