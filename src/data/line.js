95631d966d8ad8add0b921d0b2e1ebfd1d242a69
Alexis <61119747+Alexis06030631@users.noreply.github.com>
2022-07-21 11:05:09 +0200

    line function get vehicles

--- src/data/line.js
+++ src/data/line.js
@@ -1,3 +1,5 @@
+const Vehicle = require("./vehicle");
+const utils = require("../utils/utils");
 module.exports = class Line {
     #token
     constructor(data, token) {
@@ -9,5 +11,26 @@ module.exports = class Line {
         this.opening_time = data.opening_time
         this.name = data.name
         this.id = data.id
+    }
+
+    /**
+     * Get the vehicles of the line at a given time
+     * @param {string||Date||number} [since_date] defines the start date to search for vehicles
+     * @param {string||Date||number} [until_date] defines the end date to search for vehicles
+     * @param {number} [count=10] The number of vehicles to get
+     * @returns {Promise<Vehicle[]>}
+     */
+    async vehicle_journeys(since_date, until_date, count= 10) {
+        let {since, until} = utils.date_options(since_date, until_date);
+
+        return this._VehicleMany(await utils.request(this.#token, `lines/${this.id}/vehicle_journeys?since=${since}&until=${until}&count=${count}`))
+    }
+
+    _VehicleMany(vehicles) {
+        const vehiclesMany = [];
+        for(let vehicle of vehicles.vehicle_journeys) {
+            vehiclesMany.push(new Vehicle(vehicle, this.#token))
+        }
+        return vehiclesMany
     }
 }
