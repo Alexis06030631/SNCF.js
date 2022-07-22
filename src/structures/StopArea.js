9b0aad48d477bc4be9fb2bc472ad5d9d98ad6770
Alexis <61119747+Alexis06030631@users.noreply.github.com>
2022-07-22 18:52:55 +0200

    add stop area structure

---
+++ src/structures/StopArea.js
@@ -0,0 +1,13 @@
+const {Client} = require("../../index");
+
+module.exports = class StopArea extends Client{
+    constructor(data) {
+        super()
+
+        this.id = data.id
+        this.name = data.name
+        this.zip_code = data.administrative_regions[0].zip_code
+        this.timezone = data.timezone
+        this.coord = data.coord
+    }
+}
