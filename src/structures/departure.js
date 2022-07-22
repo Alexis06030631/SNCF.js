e57e72384f1c1f3e34d0047902a823e0ec83ce11
Alexis <61119747+Alexis06030631@users.noreply.github.com>
2022-07-22 18:52:21 +0200

    add functions departure class

--- src/structures/departure.js
+++ src/structures/departure.js
@@ -1,13 +1,38 @@
-const Line = require("./line");
+const Client = require("../Client");
 
-module.exports = class Departure {
-    #token
-    constructor(departure, token) {
-        this.#token = token
-        this.stop_area = {
-            id: departure.stop_point.stop_area.id,
-            name: departure.stop_point.stop_area.name,
-        }
-        this.line = (typeof Line === "function")? new Line(departure.line, this.#token) : this
+module.exports = class Departure extends Client{
+    #departure
+    constructor(departure) {
+        super()
+        this.#departure = departure;
+
+        this.arrival_date_time = departure.stop_date_time.departure_date_time;
+        this.base_arrival_date_time = departure.stop_date_time.base_departure_date_time;
+
     }
+
+    /**
+     * Get the stop departure data
+     * @return {StopArea}
+     */
+    get stop_area() {
+        return new this.structures.stop_area(this.#departure.stop_point.stop_area);
+    }
+
+    /**
+     * Get the line data
+     * @return {Line}
+     */
+    get line() {
+        return new this.structures.line(this.#departure.route.line)
+    }
+
+    /**
+     * Return boolean if the train is in late
+     * @return {boolean}
+     */
+    get isLate() {
+        return this.#departure.stop_date_time.departure_date_time !== this.#departure.stop_date_time.base_departure_date_time
+    }
+
 }
