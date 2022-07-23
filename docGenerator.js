eb30f328f22daab0c14f8dae250e26977e8384e8
Alexis <61119747+Alexis06030631@users.noreply.github.com>
2022-07-23 23:32:13 +0200

    Extends documentation

--- docGenerator.js
+++ docGenerator.js
@@ -55,24 +55,76 @@ function content_creator(data, name, file){
 
     // add methods descriptions
     content += `---\n## Methods\n`;
-    const functions_content = file.replace(constructor_data, '')
+    const match = {
+        start:"\\\/\\\*\\\*\\\s\*\(\\\n\[\^\\\*\]\|\\\*\[\^\\\/\]\*\)\\\*\\\/\.\*\\\n\*\.\*",
+        end: ".*\(.*\)"
+    }
     for(let i=0; methods.length>i; i++){
-        const match = `/${methods[i]}.*\\((.*)\\).*\\n*\\{/`
-        //console.log(file.match(match))
+        let description = '';
+        let return_type = '';
+        let params = [];
+
+        let jsdocs = file.match(new RegExp(match.start+methods[i]+match.end, 'g'));
+        if(jsdocs){
+            jsdocs = jsdocs[0]
+
+            const jsdocs_filter = jsdocs.match(/\s\*\s([^].*)/g)
+
+            for(let p=0; jsdocs_filter.length>p; p++){
+                if(!jsdocs_filter[p].includes('@') && !jsdocs_filter[p].includes('*/')){
+                    description = jsdocs_filter[p];
+
+                    if(description.includes('{@link')){
+                        const link = /\{@link\s([^}]*)\}/g.exec(description)
+                        description = description.replace(link[0], `[${link[1].toLowerCase()}](${link[1].toString()})`)
+                    }
+                }else if(jsdocs_filter[p].includes('@return')) {
+                    return_type = jsdocs_filter[p].match(/@.*\{(.*)((\<.*\>)?)\}\s?(.*)/)[1].match(/(?:(.*)<((\w*).*)>)?.*/)
+                    if(type_of.includes(return_type[1]?.toLowerCase())) {
+                        return_type[1] = return_type[1]?.toLowerCase()
+                    }else{
+                        return_type[1] = `[${return_type[1] || return_type[0]}](${return_type[1] || return_type[0]})`
+                    }
+
+                    if(return_type[3]){
+                        return_type[1] += `<[${return_type[3]}](../structures/${return_type[3].match(/\w*/)[1] || return_type[3]})>`
+                    }
+                    return_type = return_type[1]
+                }else if(jsdocs_filter[p].includes('@param')){
+                    const param_type = jsdocs_filter[p].match(/.*{(.*)}\s?(\S*)?\s?(.*)/)
+                    if(param_type){
+                        params.push({
+                            name: param_type[2].replace('[', '').replace(']', ''),
+                            type: param_type[1],
+                            description: param_type[3],
+                            optional: param_type[2].includes('[')
+                        })
+                    }else {
+                        throw new Error(`Error in param ${jsdocs_filter[p]}, in file ${data.name}`)
+                    }
+                }
+            }
+        }
+
         content += `## .${methods[i]}\n\n`
         // Set return type
-        content += `=== ${methods[i]} : \`string\`\n\n`
+        content += `=== ${methods[i]}()\n\n`
         // Set description
-        content += `${methods[i]} description.\n\n`
+        content += `${description}\n\n`
         // add parameters
-        content += `Parameters | Description\n`
-        content += "--- | ---\n"
-        content += "`count` | description.\n"
+        if(params.length>0){
+            content += `| PARAMETER | TYPE | OPTIONAL | DESCRIPTION |\n`
+            content += "| --- | --- | :---: | --- |\n"
+            for(let p=0; params.length>p; p++){
+                content += `| ${params[p].name} | ${params[p].type.replaceAll('||',';')} | ${params[p].optional?':icon-check:': ':icon-x:'} | ${params[p].description} |\n`
+            }
+        }
         // Set example
         content += "\n"
-        content += "```javascript Example.js\n"
+        content += "```javascript\n"
         content += `${methods[i]}()\n`
         content += "```\n"
+        content += `**Type: ${return_type|| 'null'}**\n\n`
         content += "===\n\n"
     }
 
