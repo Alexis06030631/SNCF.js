b4f006b533da2e464b2d55ab4240d4c768b2a70c
Alexis <61119747+Alexis06030631@users.noreply.github.com>
2022-09-21 20:27:09 +0200

    gitignore

--- test.js
+++ test.js
@@ -3,11 +3,12 @@ require("dotenv").config();
 const sncf = new Client()
 
 // Connect to sncf API with your credentials
-sncf.login(process.env.TOKEN_SNCF).then(_ =>{
+sncf.login(process.env.SNCF_TOKEN).then(_ =>{
 
     // Get all stations including the name "Paris
-    sncf.places.search('Paris').then(async place => {
-        console.log(place[0])
+    sncf.lines.search('Toulouse matabiau - montauban').then(async place => {
+        const vehicles = await place[0].vehicle_journeys(new Date(2022, 8, 22, 7), new Date(2022, 8, 22, 8), 5)
+        console.log(vehicles.filter(v => v.direction.train_direction === 'backward')[0].stop_times)
     })
 
 }).catch(err => {
