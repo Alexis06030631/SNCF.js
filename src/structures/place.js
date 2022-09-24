ca3c374df18d2aded6344595198fd28148dd3e91
Alexis <61119747+Alexis06030631@users.noreply.github.com>
2022-09-24 16:40:55 +0200

    place vehicle journeys

--- src/structures/place.js
+++ src/structures/place.js
@@ -50,4 +50,27 @@ module.exports = class Place extends Client{
             return new Error(Error.code.INVALID_EMBEDDED_TYPE)
         }else return this.#data.stop_point
     }
+
+    /**
+     * Return the vehicle journeys of this place
+     * @param {string||Date||number} [since_date] defines the start date to search for vehicles
+     * @param {string||Date||number} [until_date] defines the end date to search for vehicles
+     * @param {number} [count=10] The number of vehicles to get
+     * @returns {Promise<Vehicle[]>}
+     */
+    async vehicle_journeys(since_date, until_date, count= 10) {
+        let paramsDates = this.utils.date_options(since_date, until_date);
+        let stopParams = {name: '', value:''}
+
+        if(this.#data.embedded_type === "stop_area") {
+            stopParams.name = "stop_areas";
+            stopParams.value = this.#data.stop_area.id
+        }else if(this.#data.embedded_type === "stop_point") {
+            stopParams.name = "stop_points";
+            stopParams.value = this.#data.stop_point.id
+        }else throw new Error(Error.code.INVALID_EMBEDDED_TYPE)
+
+        let data = await this.utils.request(`${stopParams.name}/${stopParams.value}/vehicle_journeys?${paramsDates}&count=${count}`)
+        return data.vehicle_journeys.map(vehicle => new this.structures.vehicle(vehicle));
+    }
 }
