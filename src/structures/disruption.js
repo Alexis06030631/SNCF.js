5ae84e2c2ece07ab87c6d5019403d6f215c85aa1
Alexis <61119747+Alexis06030631@users.noreply.github.com>
2022-09-25 23:33:49 +0200

    fix disruption structure && add datas

--- src/structures/disruption.js
+++ src/structures/disruption.js
@@ -1,10 +1,12 @@
 const Client = require("../managers/ClientManager");
+const moment = require("moment");
 
 module.exports = class Disruption extends Client{
-    #impacted_objects
+    #data
 
     constructor(data) {
         super()
+        this.#data = data
 
         /**
          * Return the id of the disruption
@@ -25,28 +27,10 @@ module.exports = class Disruption extends Client{
         this.cause = data.cause
 
         /**
-         * Return the routes of the disruption
-         * @returns {object}
-         */
-        this.routes = data.routes
-
-        /**
-         * Return the severity of the disruption
-         * @returns {object}
-         */
-        this.severity = data.severity
-
-        /**
-         * Return the periods of the disruption
-         * @returns {array}
-         */
-        this.periods = data.application_periods
-
-        /**
          * Return the date on which the disruption was updated
          * @returns {string}
          */
-        this.updated_at = data.updated_at
+        this.updated_at = this.utils.navitia_date(data.updated_at).toDate()
 
         /**
          * Return the list messages of the raison of this disruption
@@ -54,7 +38,41 @@ module.exports = class Disruption extends Client{
          */
         this.messages = data.messages?.map(e => e.text) || []
 
-        this.#impacted_objects = data.impacted_objects
+        /**
+         * Return if the journey is canceled or not
+         * @returns {boolean}
+         */
+        this.canceled = data.severity?.name?.includes('canceled')
+
+        /**
+         * Return if the journey is delayed or not
+         * @returns {boolean}
+         */
+        this.delayed = data.severity?.name?.includes('delayed')
+
+        /**
+         * Return the delay at the departure
+         * @returns {string}
+         */
+        this.departure_delay = this.delayed? this.impacted_stops[0].departure.delay : null
+
+        /**
+         * Return the delay at the arrival
+         * @returns {string}
+         */
+        this.arrival_delay = this.delayed? this.impacted_stops[this.impacted_stops.length - 1].arrival.delay : null
+
+        /**
+         * Return the delay of the disruption
+         * @returns {string}
+         */
+        this.delay = null
+
+        /**
+         * Return if the journey the severity effect
+         * @returns {string}
+         */
+        this.effect = data.severity?.effect
     }
 
     /**
@@ -62,10 +80,43 @@ module.exports = class Disruption extends Client{
      * @returns {array}
      */
     get impacted_objects() {
-        const impacted_objects = []
-        this.#impacted_objects.forEach(impacted_object => {
-            impacted_objects.push({id: impacted_object.pt_object.id, quality: impacted_object.pt_object.quality, name: impacted_object.pt_object.trip.name})
+        return this.#data.impacted_objects.map(impacted_object => {
+            return {
+                id: impacted_object.pt_object.id,
+                trip_id: impacted_object.pt_object.trip?.name,
+                type: impacted_object.pt_object.embedded_type
+            }
         })
-        return impacted_objects
+    }
+
+    /**
+     * Return the list of impacted stops
+     * @returns {array}
+     */
+    get impacted_stops() {
+        const impacted_stops = []
+        if(this.canceled) return {error: 'This trip is canceled'}
+        this.#data.impacted_objects.forEach(impacted_object => {
+            impacted_object.impacted_stops.forEach(impacted_stop => {
+                impacted_stops.push(
+                    {
+                        id: impacted_stop.stop_point.id,
+                        name: impacted_stop.stop_point.name,
+                        arrival: {
+                            base: moment(impacted_stop.base_arrival_time, 'HHmmss').format('HH:mm:ss'),
+                            realtime: moment(impacted_stop.amended_arrival_time, 'HHmmss').format('HH:mm:ss'),
+                            delay: moment.utc(moment(impacted_stop.amended_departure_time, 'HHmmss').diff(moment(impacted_stop.base_departure_time, 'HHmmss'))).format('HH:mm:ss')
+                        },
+                        departure: {
+                            base: moment(impacted_stop.base_departure_time, 'HHmmss').format('HH:mm:ss'),
+                            realtime: moment(impacted_stop.amended_departure_time, 'HHmmss').format('HH:mm:ss'),
+                            delay: moment.utc(moment(impacted_stop.amended_departure_time, 'HHmmss').diff(moment(impacted_stop.base_departure_time, 'HHmmss'))).format('HH:mm:ss')
+                        },
+                        cause: impacted_stop.cause,
+                    }
+                )
+            })
+        })
+        return impacted_stops
     }
 }
