f19753f59c4cdfacb9298562d9055922ba205db3
Alexis <61119747+Alexis06030631@users.noreply.github.com>
2022-09-26 14:20:42 +0200

    stop time rework

--- src/structures/stop_time.js
+++ src/structures/stop_time.js
@@ -10,43 +10,26 @@ module.exports = class StopTime extends Client {
          * Return the id of the stop point
          * @returns {string}
          */
-        this.id = data.stop_point.id
+        this.id = data?.stop_point?.id
 
         /**
          * Return the name of the stop point
          * @returns {string}
          */
-        this.name = data.stop_point.name
+        this.name = data?.stop_point?.name
 
-
-        this.departure = {}
 
         /**
          * Return the departure time of the train stop
          * @returns {date}
          */
-        this.departure.date = this.#transformDate(data.departure_time)
-
-        /**
-         * Return the departure UTC time of the train stop
-         * @returns {date}
-         */
-        this.departure.UTC_date = this.#transformDate(data.utc_departure_time)
-
-
-        this.arrival = {}
+        this.departure_date = this.#transformDate(data.departure_time)
 
         /**
          * Return the arrival time of the train stop
          * @returns {date}
          */
-        this.arrival.date = this.#transformDate(data.arrival_time)
-
-        /**
-         * Return the arrival UTC time of the train stop
-         * @returns {date}
-         */
-        this.arrival.UTC_date = this.#transformDate(data?.utc_arrival_time)
+        this.arrival_date = this.#transformDate(data.arrival_time)
     }
 
     get #transformDate() {
