ecbc2e75f276ddd97c4c61ba69e03cf3d1dd37c9
Alexis <61119747+Alexis06030631@users.noreply.github.com>
2022-09-26 14:21:03 +0200

    journey details

--- src/structures/journey.js
+++ src/structures/journey.js
@@ -1,5 +1,6 @@
 const Client = require("../managers/ClientManager");
 const moment = require("moment");
+const Departure = require("./departure");
 
 module.exports = class Journey extends Client{
     #data
@@ -30,25 +31,54 @@ module.exports = class Journey extends Client{
         this.duration = moment.utc(data.duration*1000).format('HH:mm:ss');
 
         /**
-         * Return the journey's departure date
-         * @returns {date}
+         * Return the journey's departure
          */
-        this.departure_date = this.utils.navitia_date(data.sections.find(s => !!s.display_informations).departure_date_time).toDate();
+        this.departure = {
+            /**
+             * Return the journey's departure date
+             * @returns {Date}
+             */
+            date: this.utils.navitia_date(data.sections.find(s => !!s.display_informations).departure_date_time).toDate(),
+            /**
+             * Return the journey's departure stop area
+             * @returns {StopArea}
+             */
+            stop_area: new this.structures.stop_area(data.sections[0].from.stop_area)
+        }
 
         /**
-         * Return the journey's arrival date
-         * @returns {date}
+         * Return the journey's arrival
+         * @returns {object}
          */
-        this.arrival_date = this.utils.navitia_date(data.sections.find(s => !!s.display_informations).arrival_date_time).toDate();
+        this.arrival = {
+            /**
+             * Return the journey's arrival date
+             * @returns {Date}
+             */
+            date: this.utils.navitia_date(data.sections.find(s => !!s.display_informations).arrival_date_time).toDate(),
+            /**
+             * Return the journey's arrival stop area
+             * @returns {StopArea}
+             */
+            stop_area: new this.structures.stop_area(data.sections[data.sections.length-1].to.stop_area)
+        }
+
+        /**
+         * Return if the journey as disruptions
+         * @returns {boolean}
+         */
+        this.has_disruptions = data.disruptions?.length > 0;
     }
 
     /**
      * Return train's information disruptions, if the train is canceled or not, if it's delayed or not
+     * @param {string} stop_area - The stop area id to get disruptions
      * @returns {Disruption[]}
      */
-    get disruption() {
+    disruption(stop_area) {
         const disruptions = this.#data.disruptions;
-        if(disruptions?.length === 0) return null;
+        if(!disruptions?.length) return [];
+        if(stop_area) return disruptions.filter(d => d.stop_areas.find(s => s.id === stop_area.id));
         return disruptions;
     }
 }
