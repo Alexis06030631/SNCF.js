7adf14c4b0f4411470a8d59de9a7b28006aef186
Alexis <61119747+Alexis06030631@users.noreply.github.com>
2022-12-22 19:50:48 +0100

    Fix StopTime structure

--- src/structures/StopTime.js
+++ src/structures/StopTime.js
@@ -1,40 +1,52 @@
-const Client = require("../managers/ClientManager");
-const moment = require("moment");
-module.exports = class StopTime extends Client {
-    #data
-    constructor(data) {
-        super()
-        this.#data = data
-
-        /**
-         * Return the id of the stop point
-         * @returns {string}
-         */
-        this.id = data?.stop_point?.id
-
-        /**
-         * Return the name of the stop point
-         * @returns {string}
-         */
-        this.name = data?.stop_point?.name
+const StructuresManager = require("./StructuresManager");
+const {navitiaDateToDate, hourNativiaToHour} = require("../util/Converter");
+module.exports = class StopTime extends StructuresManager {
+	constructor(Client, data) {
+		super()
+		Object.defineProperty(this, "client", {value: Client})
 
 
-        /**
-         * Return the departure time of the train stop
-         * @returns {date}
-         */
-        this.departure_date = this.#transformDate(data.departure_time)
+		/**
+		 * Return the headsign of the train
+		 * @returns {string}
+		 */
+		this.headsign = data.headsign
 
-        /**
-         * Return the arrival time of the train stop
-         * @returns {date}
-         */
-        this.arrival_date = this.#transformDate(data.arrival_time)
-    }
+		/**
+		 * Arrival time
+		 * @returns {string}
+		 */
+		this.arrival_time = hourNativiaToHour(data.arrival_time)
 
-    get #transformDate() {
-        return (date) => {
-            return moment(this.#data.date + ' ' + date, 'YYYY-MM-DD  HHmmss').toDate()
-        }
-    }
+		/**
+		 * Departure time
+		 * @returns {string}
+		 */
+		this.departure_time = hourNativiaToHour(data.departure_time)
+
+		/**
+		 * Return boolean if the stop is skipped
+		 * @returns {boolean}
+		 */
+		this.skipped = data.skipped_stop
+
+		/**
+		 * Return boolean if drop off is available
+		 * @returns {boolean}
+		 */
+		this.drop_off = data.drop_off_allowed
+
+		/**
+		 * Return boolean if pick up is available
+		 * @returns {boolean}
+		 */
+		this.pick_up = data.pickup_allowed
+
+		/**
+		 * Return the stop point
+		 * @returns {StopArea}
+		 */
+		this.stop_area = new this.class_stop_area(this.client, data.stop_point)
+
+	}
 }
