d251dd6af4f991d48a1d2a98904d47456a44eb31
Alexis <61119747+Alexis06030631@users.noreply.github.com>
2022-12-22 14:20:51 +0100

    Add route structure

---
+++ src/structures/Route.js
@@ -0,0 +1,36 @@
+const StructuresManager = require("./StructuresManager");
+module.exports = class Route extends StructuresManager{
+	constructor(Client, data) {
+		super();
+
+		/**
+		 * Return the route id
+		 * @returns {string}
+		 */
+		this.id = data.id;
+
+		/**
+		 * Return the route name
+		 * @returns {string}
+		 */
+		this.name = data.name;
+
+		/**
+		 * Return the route direction
+		 * @returns {StopArea}
+		 */
+		this.direction = new this.class_stop_area(Client, data.direction.stop_area);
+
+		/**
+		 * Return the route direction type
+		 * @returns {string}
+		 */
+		this.direction_type = data.direction_type;
+
+		/**
+		 * Physical mode of the route
+		 * @returns {array}
+		 */
+		this.physical_modes = data.physical_modes
+	}
+}
